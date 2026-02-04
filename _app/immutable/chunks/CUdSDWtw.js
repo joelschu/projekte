52083f6b8d0ce278578ab771331a248ca692f03e
joelschu <joelschuelin@gmail.com>
2026-02-04 16:24:02 +0100

    Updates

--- _app/immutable/chunks/CUdSDWtw.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_d2gauo?.base??"",a=globalThis.__sveltekit_d2gauo?.assets??s??"";export{a,s as b};
---
+++ _app/immutable/chunks/DyvvdVZ0.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_1o1xja5?.base??"",a=globalThis.__sveltekit_1o1xja5?.assets??s??"";export{a,s as b};
